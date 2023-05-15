bd1d038d28b263cd2cfe2171971ca90121d233f2
Luis Victoria <x@lambda.lv>
2023-05-15 05:17:54 -0400

    Create folder functionality

--- content-script.js
+++ content-script.js
@@ -2,10 +2,10 @@ console.log('content-script.js loaded');
 
 function waitForElement(selector, callback) {
   if (document.querySelector(selector)) {
-    console.log('Element found:', selector); // Add this line
+    console.log('Element found:', selector);
     callback();
   } else {
-    console.log('Waiting for element:', selector); // Add this line
+    console.log('Waiting for element:', selector);
     setTimeout(() => waitForElement(selector, callback), 500);
   }
 }
@@ -19,11 +19,25 @@ waitForElement('.flex.flex-col.gap-2.pb-2.text-gray-100.text-sm', () => {
       const chatSelectionElement = document.querySelector('.flex.flex-col.gap-2.pb-2.text-gray-100.text-sm');
       chatSelectionElement.style.border = '3px solid red';
       const foldersElement = parser.parseFromString(html, 'text/html').querySelector('#folder-container');
-      chatSelectionElement.appendChild(foldersElement);
+      
+      // Create a MutationObserver to watch for DOM changes
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            const addedNode = mutation.addedNodes[0];
+            if (addedNode.id === 'folder-container') {
+              observer.disconnect();
+              // Call initAddFolder when folder-container is added to the DOM
+              initAddFolder();
+            }
+          }
+        });
+      });
 
-      // Add event listeners and other initialization code here
-      initSearch();
-      initAddFolder();
+      // Start observing the chatSelectionElement for DOM changes
+      observer.observe(chatSelectionElement, { childList: true });
+      
+      chatSelectionElement.appendChild(foldersElement);
     });
 });
 
@@ -41,13 +55,31 @@ function handleSearch(event) {
 
 // Initialize add folder functionality
 function initAddFolder() {
+  console.log('Initializing Add Folder button');
   const addFolderBtn = document.querySelector('#add-folder-btn');
-  addFolderBtn.addEventListener('click', handleAddFolder);
+  if (addFolderBtn) {
+    console.log('Add Folder button found');
+    addFolderBtn.addEventListener('click', handleAddFolder);
+    console.log('Add Folder button event listener added');
+  } else {
+    console.log('Add Folder button not found');
+  }
 }
 
 // Handle add folder button click
 function handleAddFolder() {
-  // Create and save a new folder
+  console.log('Add Folder button clicked');
+  // Create a new folder
+  const folder = createFolder();
+  if (folder) {
+    console.log('New folder created:', folder);
+    // Save the folder
+    saveFolder(folder);
+    // Update the UI
+    addFolderToUI(folder);
+  } else {
+    console.log('Folder creation failed');
+  }
 }
 
 // Function to handle messages from the background script
@@ -58,5 +90,32 @@ function handleMessage(request, sender, sendResponse) {
 // Add a listener for messages from the background script
 browser.runtime.onMessage.addListener(handleMessage);
 
-// ... other content script code, such as functions for linking conversations, etc.
+
+// Create a new folder
+function createFolder() {
+  const id = Date.now(); // Use the current timestamp as a unique id
+  const name = prompt('Enter folder name'); // Prompt the user to enter a folder name
+  const chats = []; // Start with an empty array of chats
+  return { id, name, chats };
+}
+
+// Save the folder
+function saveFolder(folder) {
+  // Get the current list of folders from storage
+  browser.storage.local.get('folders').then((result) => {
+    const folders = result.folders || [];
+    // Add the new folder to the list
+    folders.push(folder);
+    // Save the updated list of folders
+    browser.storage.local.set({ folders });
+  });
+}
+
+// Update the UI
+function addFolderToUI(folder) {
+  const folderListElement = document.querySelector('#folder-list');
+  const folderElement = document.createElement('div');
+  folderElement.textContent = folder.name;
+  folderListElement.appendChild(folderElement);
+}
 
